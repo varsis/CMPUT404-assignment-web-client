7f903597dca1c4541339d4bd895a7d0d66a8c743
varsis <chris@varsisstudio.com>
2015-02-02 00:29:23 -0700

    url parsing almost complete

--- httpclient.py
+++ httpclient.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 # Copyright 2013 Abram Hindle
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,11 +33,42 @@ class HTTPRequest(object):
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
+
+    __REGEX_HTTP = "^http:\/\/[A-Za-z0-9\.-]{3,}\.[A-Za-z]{3}"
+    __REGEX_URL = "^[A-Za-z0-9\.-]{3,}\.[A-Za-z]{3}"
+    __REGEX_PORT = "(?:\:[0-9]{2,4})"
+
+    def get_host_port(self,url):
+        # default port number
+        port = 80
+
+        host = re.match(self.__REGEX_HTTP,url)
+
+        if host is not None:
+            host = host.group(0)
+            host = host[7:]
+        else:
+            host = re.match(self.__REGEX_URL,url)
+            if host is not None:
+                host = host.group(0)
+
+        check_port = re.search(self.__REGEX_PORT,url)
+
+        if check_port is not None:
+            check_port = check_port.group(0)
+            port = int(check_port[1:])
+
+        return (host,port)
+
 
     def connect(self, host, port):
-        # use sockets!
-        return None
+        try:
+            userSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            userSocket.connect((host,port))
+            return userSocket
+        except Exception as e:
+            print "Address-related error connecting to server: %s" % e
+            sys.exit(1)
 
     def get_code(self, data):
         return None
@@ -47,6 +78,15 @@ class HTTPClient(object):
 
     def get_body(self, data):
         return None
+
+    def sendall(self, sock, message):
+        totalsent = 0
+        message_len = len(message)
+        while totalsent < message_len:
+            sent = sock.send(message[totalsent:])
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent = totalsent + sent
 
     # read everything from the socket
     def recvall(self, sock):
@@ -61,6 +101,10 @@ class HTTPClient(object):
         return str(buffer)
 
     def GET(self, url, args=None):
+        host_port = self.get_host_port(url)
+        socket = self.connect(host_port[0],host_port[1])
+        print(socket)
+        self.sendall(socket,"CRAP")
         code = 500
         body = ""
         return HTTPRequest(code, body)
@@ -75,7 +119,7 @@ class HTTPClient(object):
             return self.POST( url, args )
         else:
             return self.GET( url, args )
-    
+
 if __name__ == "__main__":
     client = HTTPClient()
     command = "GET"
@@ -83,6 +127,6 @@ if __name__ == "__main__":
         help()
         sys.exit(1)
     elif (len(sys.argv) == 3):
-        print client.command( sys.argv[1], sys.argv[2] )
+        print client.command( sys.argv[2], sys.argv[1] )
     else:
-        print client.command( command, sys.argv[1] )    
+        print client.command( command, sys.argv[2] )
